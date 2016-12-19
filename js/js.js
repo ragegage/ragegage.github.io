970d2991a99b5de9ca920f061f54b14c590f7d7d
Gage <gagesnewman@gmail.com>
2016-12-18 21:52:43 -0800

    add font, smooth scroll, and set up structure of site

--- js/js.js
+++ js/js.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // setTimeout(() => alert("you did enjoy it!"), 3000)
-});
+  document.getElementsByClassName('buffalo')[0].addEventListener('click', function() {
+    console.log('herererere')
+    const more = `<div class='more'>
+                    I build fullstack and frontend web apps
+                  </div>
+                  <div class='more'>
+                    and I like to have fun with it
+                  </div>`
+    document.getElementsByTagName('body')[0].innerHTML += more
+    document.querySelector('.more').scrollIntoView({
+      behavior: 'smooth'
+    })
+  }, false)
+})
