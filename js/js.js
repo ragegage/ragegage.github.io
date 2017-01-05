7a09627559d7093672264c8fbb5225f64c4a193e
Gage <gagesnewman@gmail.com>
2017-01-04 19:11:01 -0800

    update js to use new class name

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementsByClassName('buffalo')[0].addEventListener('click', function() {
     // change this to toggle a class on the body which has overflow:hidden
     document.getElementsByTagName('body')[0].removeAttribute("class")
-    document.querySelector('.more').scrollIntoView({
+    document.querySelector('.projects').scrollIntoView({
       behavior: 'smooth'
     })
   }, false)
