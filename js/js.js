9d902dc513290599db47295e96ef287b5044d369
Gage <gagesnewman@gmail.com>
2017-01-04 20:00:42 -0800

    add welcome console.logs

--- js/js.js
+++ js/js.js
@@ -1,3 +1,6 @@
+console.log("hello friend!");
+console.log("this is a simple site; not too much to see here");
+
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementsByClassName('buffalo')[0].addEventListener('click', function() {
     // change this to toggle a class on the body which has overflow:hidden
