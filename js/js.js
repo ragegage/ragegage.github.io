81dcedfac988174a570b12565763eccecc779e3b
Gage <gagesnewman@gmail.com>
2017-01-06 07:50:34 -0800

    add localStorage to js

--- js/js.js
+++ js/js.js
@@ -2,8 +2,12 @@ console.log("hello friend!");
 console.log("this is a simple site; not too much to see here");
 
 document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.visited) // if user has visited the page before, remove the usual rigamarole
+    document.getElementsByTagName('body')[0].removeAttribute("class")
+
   document.getElementsByClassName('buffalo')[0].addEventListener('click', function() {
-    // change this to toggle a class on the body which has overflow:hidden
+    localStorage.visited = true // local "cookie"-type behavior
+
     document.getElementsByTagName('body')[0].removeAttribute("class")
     document.querySelector('.projects').scrollIntoView({
       behavior: 'smooth'
